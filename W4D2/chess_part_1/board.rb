9cae21010d2f6e427f31c0caac0468f61443b436
Charis <bluegrassgal222@gmail.com>
2021-02-09 18:25:00 -0700

    Add find_king method, add NullPiece class, various changes

--- W4D2/chess_part_1/board.rb
+++ W4D2/chess_part_1/board.rb
@@ -4,7 +4,7 @@ class Board
   attr_reader :grid
 
   @@INITIATIAL_POS = {
-    "rook" => [[0, 0], [0, 7], [7, 7], [7, 7]],
+    "rook" => [[0, 0], [0, 7], [7, 0], [7, 7]],
     "knight" => [[0, 1], [0, 6], [7, 1], [7, 6]],
     "bishop" => [[0, 2],[0, 5], [7, 2], [7, 5]],
     "queen" => [[0, 3], [7, 3]],
@@ -44,7 +44,7 @@ class Board
             grid[row][col] = (row < 2 ? Piece.new(:W, k, pos) : Piece.new(:B, k, pos))
             break
           else
-            grid[row][col] = Piece.new(nil, nil, pos)
+            grid[row][col] = NullPiece.instance
           end
         end
       end
@@ -56,7 +56,11 @@ class Board
     grid.each do |row|
       row_pieces = []
       row.each do |piece|
-        row_pieces << piece
+        if !piece.name.nil?
+          row_pieces << piece.to_s
+        else
+          row_pieces << "   "
+        end
       end
       p row_pieces
     end
@@ -80,18 +84,35 @@ class Board
     end
   end
 
+  def valid_pos(pos)
+    pos[0].between?(0,7) && pos[1].between?(0,7)
+  end
+
+  def find_king(color)
+    (0...8).each do |row|
+      (0...8).each do |col|
+        return [row, col] if grid[row][col].color == color && grid[row][col].name == "king"
+      end
+    end
+    nil
+  end
+
+  def checkmate
+    
+  end
+
 end
 
 board = Board.new
 board.print
-p "-" * 60
-board.move_piece([0,0], [5,0])
-board.print
-p "-" * 60
-board.move_piece([4,1], [4,2])
-board.print
-board.move_piece([7,5], [7,4])
-board.print
-p "-" * 60
-board.move_piece([6,2], [1,2])
-board.print
+# p "-" * 60
+# board.move_piece([0,0], [5,0])
+# board.print
+# p "-" * 60
+# board.move_piece([4,1], [4,2])
+# board.print
+# board.move_piece([7,5], [7,4])
+# board.print
+# p "-" * 60
+# board.move_piece([6,2], [1,2])
+# board.print
--- W4D2/chess_part_1/piece.rb
+++ W4D2/chess_part_1/piece.rb
@@ -1,3 +1,5 @@
+require 'singleton'
+
 class Piece
   attr_reader :color, :name, :pos
   def initialize(color, name, pos)
@@ -6,8 +8,27 @@ class Piece
     @pos = pos
   end
 
-  def inspect
-    "#{@color} #{@name}".inspect
+  def to_s
+    return "#{color} #{name[0].upcase}"
   end
 
 end
+
+class NullPiece < Piece
+
+  include Singleton
+
+  def initialize
+    super(nil, nil, pos)
+  end
+
+  def moves
+    nil
+  end
+
+  def name
+    nil
+  end
+end
+
+# p n = NullPiece.instance
