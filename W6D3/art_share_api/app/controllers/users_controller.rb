a810e96e30ce3d69fae7cc65ada07e8c3aa3b981
jhnegbrt <jhnegbrt@gmail.com>
2021-02-24 17:14:00 -0800

    adds create method

--- W6D3/art_share_api/app/controllers/users_controller.rb
+++ W6D3/art_share_api/app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
   def index
     users = User.all
-
     render json: users
-  end
-
-  def create
-    render json: params
   end
 
   def show
     render json: params
+  end
+
+  def create
+
+    debugger
+    user = User.new(params.require(:user).permit(:name, :email))
+    
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
 
