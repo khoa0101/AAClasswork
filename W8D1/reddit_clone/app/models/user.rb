10da03c682fca749df8750f91bf948bd998eb644
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-03-08 09:54:23 -0800

    Define auth_features

---
+++ W8D1/reddit_clone/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.base64
+  end
+
+end
