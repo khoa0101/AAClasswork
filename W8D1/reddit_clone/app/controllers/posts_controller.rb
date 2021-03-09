b974dcc0bcaeeb9d8220c1828c21382d131e5cdb
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-03-08 17:03:17 -0800

    Complete phase 6

--- W8D1/reddit_clone/app/controllers/posts_controller.rb
+++ W8D1/reddit_clone/app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @all_comments = @post.comments.includes(:author)
+    @all_comments = @post.comments_by_parent_id
     render :show
   end
 
--- W8D1/reddit_clone/app/models/post.rb
+++ W8D1/reddit_clone/app/models/post.rb
@@ -16,4 +16,15 @@ class Post < ApplicationRecord
   has_many :comments,
     foreign_key: :post_id,
     class_name: 'Comment'
+
+  def comments_by_parent_id
+    hash = Hash.new { |hash, key| hash[key] = [] }
+
+    self.comments.includes(:author).each do |comment|
+      hash[comment.parent_comment_id] << comment
+    end
+
+    hash
+
+  end
 end
