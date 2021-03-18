ec3642aebe5e3028e932defda0b6e0915d523de3
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-03-17 17:31:16 -0700

    Partial define hanoi-view.js

---
+++ W9D3/tower-of-hanoi/dist/main.js
@@ -0,0 +1,76 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/game.js":
+/*!*********************!*\
+  !*** ./src/game.js ***!
+  \*********************/
+/***/ ((module) => {
+
+eval("class Game {\n  constructor() {\n    this.towers = [[3, 2, 1], [], []];\n  }\n\n  isValidMove(startTowerIdx, endTowerIdx) {\n      const startTower = this.towers[startTowerIdx];\n      const endTower = this.towers[endTowerIdx];\n\n      if (startTower.length === 0) {\n        return false;\n      } else if (endTower.length == 0) {\n        return true;\n      } else {\n        const topStartDisc = startTower[startTower.length - 1];\n        const topEndDisc = endTower[endTower.length - 1];\n        return topStartDisc < topEndDisc;\n      }\n  }\n\n  isWon() {\n      // move all the discs to the last or second tower\n      return (this.towers[2].length == 3) || (this.towers[1].length == 3);\n  }\n\n  move(startTowerIdx, endTowerIdx) {\n      if (this.isValidMove(startTowerIdx, endTowerIdx)) {\n        this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());\n        return true;\n      } else {\n        return false;\n      }\n  }\n\n  print() {\n      console.log(JSON.stringify(this.towers));\n  }\n\n  promptMove(reader, callback) {\n      this.print();\n      reader.question(\"Enter a starting tower: \", start => {\n        const startTowerIdx = parseInt(start);\n        reader.question(\"Enter an ending tower: \", end => {\n          const endTowerIdx = parseInt(end);\n          callback(startTowerIdx, endTowerIdx);\n        });\n      });\n  }\n\n  run(reader, gameCompletionCallback) {\n      this.promptMove(reader, (startTowerIdx, endTowerIdx) => {\n        if (!this.move(startTowerIdx, endTowerIdx)) {\n          console.log(\"Invalid move!\");\n        }\n\n        if (!this.isWon()) {\n          // Continue to play!\n          this.run(reader, gameCompletionCallback);\n        } else {\n          this.print();\n          console.log(\"You win!\");\n          gameCompletionCallback();\n        }\n      });\n  }\n}\n\nmodule.exports = Game;\n\n\n//# sourceURL=webpack:///./src/game.js?");
+
+/***/ }),
+
+/***/ "./src/hanoi-view.js":
+/*!***************************!*\
+  !*** ./src/hanoi-view.js ***!
+  \***************************/
+/***/ ((module) => {
+
+eval("function View(game, dom){\n  this.game = game;\n  this.dom = dom;\n  this.setupTowers();\n  this.render();\n}\n\nView.prototype.render = function(){\n  const ul = $(\"ul\");\n  const li = $(\"li\");\n\n  for (let i = 0; i < 3; i++)\n  {\n    for (let j = 0; j < 3; j++)\n    {\n      if (this.game.towers[i][j])\n      {\n        const rod = $(`.rod${i + 1}`);\n        \n        // .data(\"disNumber\", this.game.towers[i][j]);\n      }\n      else \n      {\n        $(`.rod${i+1} > disc${j}`).removeClass(\"hasDisc\");\n      }\n    }\n  }\n}\n\nView.prototype.setupTowers = function(){\n  const rod1 = $(\"<ul>\");\n  const rod2 = $(\"<ul>\");\n  const rod3 = $(\"<ul>\");\n  rod1.addClass(\"rod1\");\n  rod1.data(\"index\", 0);\n  rod2.addClass(\"rod2\");\n  rod2.data(\"index\", 1);\n  rod3.addClass(\"rod3\");\n  rod3.data(\"index\", 2);\n\n  this.dom.append(rod1);\n  this.dom.append(rod2);\n  this.dom.append(rod3);\n\n  for (let i = 0; i < 3; i++){\n    let li = $(\"<li>\");\n    li.addClass(`spot-${i}`)\n\n    li.data(\"index\", i);\n    rod1.append(li);\n  }\n\n  for (let i = 0; i < 3; i++){\n    let li = $(\"<li>\");\n\n    li.data(\"index\", i);\n    rod2.append(li);\n  }\n\n  for (let i = 0; i < 3; i++){\n    let li = $(\"<li>\");\n\n    li.data(\"index\", i);\n    rod3.append(li);\n  }\n}\n\nmodule.exports = View;\n\n//# sourceURL=webpack:///./src/hanoi-view.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const HanoiGame = __webpack_require__(/*! ./game.js */ \"./src/game.js\");\nconst HanoiView = __webpack_require__(/*! ./hanoi-view.js */ \"./src/hanoi-view.js\");\n\n$(() => {\n  const rootEl = $('.hanoi');\n  const game = new HanoiGame();\n  new HanoiView(game, rootEl);\n  //console.log(\"index.js\")\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
---
+++ W9D3/tower-of-hanoi/src/hanoi-view.js
@@ -0,0 +1,68 @@
+function View(game, dom){
+  this.game = game;
+  this.dom = dom;
+  this.setupTowers();
+  this.render();
+}
+
+View.prototype.render = function(){
+  const ul = $("ul");
+  const li = $("li");
+
+  for (let i = 0; i < 3; i++)
+  {
+    for (let j = 0; j < 3; j++)
+    {
+      if (this.game.towers[i][j])
+      {
+        const rod = $(`.rod${i + 1}`);
+        
+        // .data("disNumber", this.game.towers[i][j]);
+      }
+      else 
+      {
+        $(`.rod${i+1} > disc${j}`).removeClass("hasDisc");
+      }
+    }
+  }
+}
+
+View.prototype.setupTowers = function(){
+  const rod1 = $("<ul>");
+  const rod2 = $("<ul>");
+  const rod3 = $("<ul>");
+  rod1.addClass("rod1");
+  rod1.data("index", 0);
+  rod2.addClass("rod2");
+  rod2.data("index", 1);
+  rod3.addClass("rod3");
+  rod3.data("index", 2);
+
+  this.dom.append(rod1);
+  this.dom.append(rod2);
+  this.dom.append(rod3);
+
+  for (let i = 0; i < 3; i++){
+    let li = $("<li>");
+    li.addClass(`spot-${i}`)
+
+    li.data("index", i);
+    rod1.append(li);
+  }
+
+  for (let i = 0; i < 3; i++){
+    let li = $("<li>");
+
+    li.data("index", i);
+    rod2.append(li);
+  }
+
+  for (let i = 0; i < 3; i++){
+    let li = $("<li>");
+
+    li.data("index", i);
+    rod3.append(li);
+  }
+}
+
+module.exports = View;
