80b3bde1f3672ec573ad7f13743dafbc1d5c3edf
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-04-01 11:41:11 -0700

    Complete part 9

--- W11D4/skeleton/frontend/components/app.jsx
+++ W11D4/skeleton/frontend/components/app.jsx
@@ -4,13 +4,14 @@ import NavBarContainer from './nav_bar/nav_bar_container';
 import ChirpIndexContainer from './chirps/chirp_index_container';
 import SignupContainer from './session/signup_container';
 import Home from './home/home';
+import { AuthRoute, ProtectedRoute } from '../utils/route_utils'
 import { Route } from 'react-router-dom';
 
 export default () => (
   <div>
     <Route path="/" component={NavBarContainer} />
     <Route exact path="/" component={Home} />
-    <Route path="/chirps" component={ChirpIndexContainer} />
-    <Route path="/signup" component={SignupContainer} />
+    <ProtectedRoute path="/chirps" component={ChirpIndexContainer} />
+    <AuthRoute path="/signup" component={SignupContainer} />
   </div>
 );
