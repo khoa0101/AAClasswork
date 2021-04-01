7b664e1d3e7f58196b7c1425cd39eea11363b287
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-04-01 10:34:41 -0700

    Complete part 5

--- W11D4/skeleton/frontend/components/session/signup.jsx
+++ W11D4/skeleton/frontend/components/session/signup.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
 
 class Signup extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+    }
+
+    this.handleSubmit = this.handleSubmit();
+  }
+
+  handleInput(type){
+    return (e) => {
+      this.setState({ [type]: e.target.value });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createNewUser(this.state)
+      .then(() => this.props.history.push('/chirps'));
+  }
+
   render () {
     return (
-    <div></div>
+    <div className='session-form'>
+      <h2>Sign Up!</h2>
+        <form>
+          <label>Username
+            <input
+              type='text'
+              value={this.state.username}
+              onChanged={this.handleInput('username')}
+            />
+          </label>
+          <label>Email
+            <input
+              type='text'
+              value={this.state.email}
+              onChanged={this.handleInput('email')}
+            />
+          </label>
+          <label>Password
+            <input
+              type='password'
+              value={this.state.password}
+              onChanged={this.handleInput('password')}
+            />
+          </label>
+          <button onChick={this.handleSubmit()}>Sign Up</button>
+        </form>
+    </div>
     );
   }
 };
