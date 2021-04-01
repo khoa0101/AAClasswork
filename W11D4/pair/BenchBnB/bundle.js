526ac83b9c1a340af803dcb207385ab940c64925
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-04-01 14:16:39 -0700

    Set up BenchBnB

---
+++ W11D4/pair/BenchBnB/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./bench_bnb.jsx":
+/*!***********************!*\
+  !*** ./bench_bnb.jsx ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module 'babel-preset-env'\n- Did you mean \"@babel/env\"?\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:580:15)\n    at resolve (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/v8-compile-cache/v8-compile-cache.js:164:23)\n    at resolveStandardizedName (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/@babel/core/lib/config/files/plugins.js:96:7)\n    at resolvePreset (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/@babel/core/lib/config/files/plugins.js:44:10)\n    at loadPreset (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/@babel/core/lib/config/files/plugins.js:63:20)\n    at loadPreset.next (<anonymous>)\n    at createDescriptor (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/@babel/core/lib/config/config-descriptors.js:179:16)\n    at createDescriptor.next (<anonymous>)\n    at step (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/gensync/index.js:261:32)\n    at evaluateAsync (/home/khoa/AAClasswork/W11D4/BenchBnB/node_modules/gensync/index.js:291:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./bench_bnb.jsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
